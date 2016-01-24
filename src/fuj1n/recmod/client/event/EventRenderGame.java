a74393bb27ee8c0780db5319bc4fb70b5e608d49
Arthur Uzulin <fuj1nman@hotmail.com>
2016-01-24 15:46:47 +1100

    Rewrote in-game renderer, fixes #6

--- src/fuj1n/recmod/client/event/EventRenderGame.java
+++ src/fuj1n/recmod/client/event/EventRenderGame.java
@@ -1,8 +1,8 @@
 package fuj1n.recmod.client.event;
 
+import com.mojang.authlib.GameProfile;
 import fuj1n.recmod.RecMod;
 import fuj1n.recmod.lib.IndexReference;
-import java.util.Iterator;
 import java.util.List;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
@@ -24,6 +24,10 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EventRenderGame extends Gui {
 
+  private static final int INDICATOR_WIDTH = 8;
+  private static final int INDICATOR_PADDING = 0;
+  private static final int INDICATOR_TOTAL = INDICATOR_WIDTH * 2 + INDICATOR_PADDING * 2;
+
   private ResourceLocation indicatorsHigh = new ResourceLocation("recmod:textures/sheets/indicatorsx2.png");
   private ResourceLocation indicatorsLow = new ResourceLocation("recmod:textures/sheets/indicators.png");
 
@@ -31,171 +35,182 @@ public class EventRenderGame extends Gui {
 
   @SubscribeEvent
   public void preRenderGameOverlay(RenderGameOverlayEvent.Pre event) {
+    Minecraft mc = Minecraft.getMinecraft();
+    ResourceLocation indicators = Minecraft.isFancyGraphicsEnabled() ? indicatorsHigh : indicatorsLow;
+
     if (event.type == ElementType.PLAYER_LIST) {
       event.setCanceled(true);
-      renderGameOverlay(event);
+
+      to = mc.ingameGUI.getTabList();
+
+      renderPlayerList(event, mc, indicators);
+    }
+  }
+
+  public void renderPlayerList(RenderGameOverlayEvent.Pre event, Minecraft mc, ResourceLocation indicators) {
+    int width = event.resolution.getScaledWidth();
+    Scoreboard scoreboardIn = mc.theWorld.getScoreboard();
+    ScoreObjective scoreObjectiveIn = scoreboardIn.getObjectiveInDisplaySlot(0);
+
+    NetHandlerPlayClient nethandlerplayclient = mc.thePlayer.sendQueue;
+    List<NetworkPlayerInfo> list = to.field_175252_a.<NetworkPlayerInfo>sortedCopy(nethandlerplayclient.getPlayerInfoMap());
+
+    int namePingGap = 0;
+    int j = 0;
+
+    for (NetworkPlayerInfo networkplayerinfo : list) {
+      int nameWidth = mc.fontRendererObj.getStringWidth(to.getPlayerName(networkplayerinfo));
+      namePingGap = Math.max(namePingGap, nameWidth);
+
+      if (scoreObjectiveIn != null && scoreObjectiveIn.getRenderType() != IScoreObjectiveCriteria.EnumRenderType.HEARTS) {
+        nameWidth = mc.fontRendererObj.getStringWidth(" " + scoreboardIn.getValueFromObjective(networkplayerinfo.getGameProfile().getName(), scoreObjectiveIn).getScorePoints());
+        j = Math.max(j, nameWidth);
+      }
+    }
+
+    list = list.subList(0, Math.min(list.size(), 80));
+    int l3 = list.size();
+    int i4 = l3;
+    int userRows;
+
+    for (userRows = 1; i4 > 20; i4 = (l3 + userRows - 1) / userRows) {
+      ++userRows;
+    }
+
+    boolean flag = mc.isIntegratedServerRunning() || mc.getNetHandler().getNetworkManager().getIsencrypted();
+    int l;
+
+    if (scoreObjectiveIn != null) {
+      if (scoreObjectiveIn.getRenderType() == IScoreObjectiveCriteria.EnumRenderType.HEARTS) {
+        l = 90;
+      } else {
+        l = j;
+      }
+    } else {
+      l = 0;
+    }
+
+    int userWidth = Math.min(userRows * ((flag ? 9 : 0) + namePingGap + l + 13), width - 50) / userRows;
+    int userX = width / 2 - (userWidth * userRows + (userRows - 1) * 5) / 2;
+    int menuY = 10;
+    int menuWidth = userWidth * userRows + (userRows - 1) * 5;
+    List<String> list1 = null;
+    List<String> list2 = null;
+
+    if (to.header != null) {
+      list1 = mc.fontRendererObj.listFormattedStringToWidth(to.header.getFormattedText(), width - 50);
+
+      for (String s : list1) {
+        menuWidth = Math.max(menuWidth, mc.fontRendererObj.getStringWidth(s));
+      }
+    }
+
+    if (to.footer != null) {
+      list2 = mc.fontRendererObj.listFormattedStringToWidth(to.footer.getFormattedText(), width - 50);
+
+      for (String s2 : list2) {
+        menuWidth = Math.max(menuWidth, mc.fontRendererObj.getStringWidth(s2));
+      }
+    }
+
+    // Indicator space transforms
+    userWidth += INDICATOR_TOTAL;
+    userX -= INDICATOR_TOTAL * userRows / 2;
+    menuWidth += INDICATOR_TOTAL * userRows;
+
+    if (list1 != null) {
+      drawRect(width / 2 - menuWidth / 2 - 1, menuY - 1, width / 2 + menuWidth / 2 + 1, menuY + list1.size() * mc.fontRendererObj.FONT_HEIGHT, Integer.MIN_VALUE);
+
+      for (String s3 : list1) {
+        int i2 = mc.fontRendererObj.getStringWidth(s3);
+        mc.fontRendererObj.drawStringWithShadow(s3, (float) (width / 2 - i2 / 2), (float) menuY, -1);
+        menuY += mc.fontRendererObj.FONT_HEIGHT;
+      }
+
+      ++menuY;
+    }
+
+    drawRect(width / 2 - menuWidth / 2 - 1, menuY - 1, width / 2 + menuWidth / 2 + 1, menuY + i4 * 9, Integer.MIN_VALUE);
+
+    for (int k4 = 0; k4 < l3; ++k4) {
+      int l4 = k4 / i4;
+      int i5 = k4 % i4;
+      int j2 = userX + l4 * userWidth + l4 * 5;
+      int k2 = menuY + i5 * 9;
+      drawRect(j2, k2, j2 + userWidth, k2 + 8, 553648127);
+      GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+      GlStateManager.enableAlpha();
+      GlStateManager.enableBlend();
+      GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+
+      if (k4 < list.size()) {
+        NetworkPlayerInfo networkplayerinfo1 = (NetworkPlayerInfo) list.get(k4);
+        String s1 = to.getPlayerName(networkplayerinfo1);
+        GameProfile gameprofile = networkplayerinfo1.getGameProfile();
+
+        if (flag) {
+          EntityPlayer entityplayer = mc.theWorld.getPlayerEntityByUUID(gameprofile.getId());
+          boolean flag1 = entityplayer != null && entityplayer.isWearing(EnumPlayerModelParts.CAPE) && (gameprofile.getName().equals("Dinnerbone") || gameprofile.getName().equals("Grumm"));
+          mc.getTextureManager().bindTexture(networkplayerinfo1.getLocationSkin());
+          int l2 = 8 + (flag1 ? 8 : 0);
+          int i3 = 8 * (flag1 ? -1 : 1);
+          Gui.drawScaledCustomSizeModalRect(j2, k2, 8.0F, (float) l2, 8, i3, 8, 8, 64.0F, 64.0F);
+
+          if (entityplayer != null && entityplayer.isWearing(EnumPlayerModelParts.HAT)) {
+            int j3 = 8 + (flag1 ? 8 : 0);
+            int k3 = 8 * (flag1 ? -1 : 1);
+            Gui.drawScaledCustomSizeModalRect(j2, k2, 40.0F, (float) j3, 8, k3, 8, 8, 64.0F, 64.0F);
+          }
+
+          j2 += 9;
+        }
+
+        if (networkplayerinfo1.getGameType() == WorldSettings.GameType.SPECTATOR) {
+          s1 = EnumChatFormatting.ITALIC + s1;
+          mc.fontRendererObj.drawStringWithShadow(s1, (float) j2, (float) k2, -1862270977);
+        } else {
+          mc.fontRendererObj.drawStringWithShadow(s1, (float) j2, (float) k2, -1);
+        }
+
+        // Indicator rendering
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        mc.getTextureManager().bindTexture(indicators);
+        int indicatorRecIndex = RecMod.instance.isPlayerRecording(networkplayerinfo1.getGameProfile().getName()) ? IndexReference.ICON_RED_INDEX : IndexReference.ICON_GRAY_INDEX;
+        int indicatorStrIndex = RecMod.instance.isPlayerStreaming(networkplayerinfo1.getGameProfile().getName()) ? IndexReference.ICON_GREEN_INDEX : IndexReference.ICON_GRAY_INDEX;
+        drawTexturedModalRect(j2 - (flag ? 9 : 0) + userWidth - INDICATOR_TOTAL + INDICATOR_PADDING, k2, indicatorRecIndex * 8, (int) Math.floor(indicatorRecIndex / 32) * 8 + IndexReference.RES_SSD, INDICATOR_WIDTH, INDICATOR_WIDTH);
+        drawTexturedModalRect(j2 - (flag ? 9 : 0) + userWidth - INDICATOR_TOTAL + INDICATOR_PADDING * 2 + INDICATOR_WIDTH, k2, indicatorStrIndex * 8, (int) Math.floor(indicatorStrIndex / 32) * 8 + IndexReference.RES_SSD, INDICATOR_WIDTH, INDICATOR_WIDTH);
+
+        if (scoreObjectiveIn != null && networkplayerinfo1.getGameType() != WorldSettings.GameType.SPECTATOR) {
+          int k5 = j2 + namePingGap + 1;
+          int l5 = k5 + l;
+
+          if (l5 - k5 > 5) {
+            to.drawScoreboardValues(scoreObjectiveIn, k2, gameprofile.getName(), k5, l5, networkplayerinfo1);
+          }
+        }
+
+        to.drawPing(userWidth - INDICATOR_TOTAL, j2 - (flag ? 9 : 0), k2, networkplayerinfo1);
+      }
+    }
+
+    if (list2 != null) {
+      menuY = menuY + i4 * 9 + 1;
+      drawRect(width / 2 - menuWidth / 2 - 1, menuY - 1, width / 2 + menuWidth / 2 + 1, menuY + list2.size() * mc.fontRendererObj.FONT_HEIGHT, Integer.MIN_VALUE);
+
+      for (String s4 : list2) {
+        int j5 = mc.fontRendererObj.getStringWidth(s4);
+        mc.fontRendererObj.drawStringWithShadow(s4, (float) (width / 2 - j5 / 2), (float) menuY, -1);
+        menuY += mc.fontRendererObj.FONT_HEIGHT;
+      }
     }
   }
 
   @SubscribeEvent
-  public void renderGameOverlay(RenderGameOverlayEvent event) {
+  public void renderGameOverlay(RenderGameOverlayEvent.Post event) {
     Minecraft mc = Minecraft.getMinecraft();
-    ResourceLocation indicators = mc.isFancyGraphicsEnabled() ? indicatorsHigh : indicatorsLow;
+    ResourceLocation indicators = Minecraft.isFancyGraphicsEnabled() ? indicatorsHigh : indicatorsLow;
 
-    if (event.type == ElementType.PLAYER_LIST) {
-      if (to == null) {
-        to = Minecraft.getMinecraft().ingameGUI.getTabList();
-      }
-
-      int infooffset = 1;
-
-      int width = event.resolution.getScaledWidth();
-      Scoreboard scoreboard = mc.theWorld.getScoreboard();
-      ScoreObjective scoreobjective = mc.theWorld.getScoreboard().getObjectiveInDisplaySlot(0);
-
-      NetHandlerPlayClient nethandlerplayclient = mc.thePlayer.sendQueue;
-      List list = to.field_175252_a.sortedCopy(nethandlerplayclient.getPlayerInfoMap());
-      int j = 0;
-      int k = 0;
-      Iterator iterator = list.iterator();
-      int l;
-
-      while (iterator.hasNext()) {
-        NetworkPlayerInfo networkplayerinfo = (NetworkPlayerInfo) iterator.next();
-        l = mc.fontRendererObj.getStringWidth(to.getPlayerName(networkplayerinfo));
-        j = Math.max(j, l) + 24;
-        k = Math.max(k, l);
-      }
-
-      list = list.subList(0, Math.min(list.size(), 80));
-      int j3 = list.size();
-      int k3 = j3;
-
-      for (l = 1; k3 > 20; k3 = (j3 + l - 1) / l) {
-        ++l;
-      }
-
-      boolean flag = mc.isIntegratedServerRunning() || mc.getNetHandler().getNetworkManager().getIsencrypted();
-      int i1;
-
-      if (scoreobjective != null) {
-        if (scoreobjective.getRenderType() == IScoreObjectiveCriteria.EnumRenderType.HEARTS) {
-          i1 = 90;
-        } else {
-          i1 = k;
-        }
-      } else {
-        i1 = 0;
-      }
-
-      int j1 = Math.min(l * ((flag ? 9 : 0) + j + i1 + 13), width - 50) / l;
-      int k1 = width / 2 - (j1 * l + (l - 1) * 5) / 2;
-      int l1 = 10;
-      int i2 = j1 * l + (l - 1) * 5;
-      List list1 = null;
-      List list2 = null;
-      Iterator iterator1;
-      String s;
-
-      if (to.header != null) {
-        list1 = mc.fontRendererObj.listFormattedStringToWidth(to.header.getFormattedText(), width - 50);
-
-        for (iterator1 = list1.iterator(); iterator1.hasNext(); i2 = Math.max(i2, mc.fontRendererObj.getStringWidth(s))) {
-          s = (String) iterator1.next();
-        }
-      }
-
-      if (to.footer != null) {
-        list2 = mc.fontRendererObj.listFormattedStringToWidth(to.footer.getFormattedText(), width - 50);
-
-        for (iterator1 = list2.iterator(); iterator1.hasNext(); i2 = Math.max(i2, mc.fontRendererObj.getStringWidth(s))) {
-          s = (String) iterator1.next();
-        }
-      }
-
-      int j2;
-
-      if (list1 != null) {
-        drawRect(width / 2 - i2 / 2 - 1, l1 - 1, width / 2 + i2 / 2 + 1, l1 + list1.size() * mc.fontRendererObj.FONT_HEIGHT, Integer.MIN_VALUE);
-
-        for (iterator1 = list1.iterator(); iterator1.hasNext(); l1 += mc.fontRendererObj.FONT_HEIGHT) {
-          s = (String) iterator1.next();
-          j2 = mc.fontRendererObj.getStringWidth(s);
-          mc.fontRendererObj.drawStringWithShadow(s, (float) (width / 2 - j2 / 2), (float) l1, -1);
-        }
-
-        ++l1;
-      }
-
-      drawRect(width / 2 - i2 / 2 - 1, l1 - 1, width / 2 + i2 / 2 + 1, l1 + k3 * 9, Integer.MIN_VALUE);
-
-      for (int l3 = 0; l3 < j3; ++l3) {
-        int i4 = l3 / k3;
-        j2 = l3 % k3;
-        int k2 = k1 + i4 * j1 + i4 * 5;
-        int l2 = l1 + j2 * 9;
-        drawRect(k2, l2, k2 + j1, l2 + 8, 553648127);
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        GlStateManager.enableAlpha();
-        GlStateManager.enableBlend();
-        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-
-        if (l3 < list.size()) {
-          NetworkPlayerInfo networkplayerinfo1 = (NetworkPlayerInfo) list.get(l3);
-          String s1 = to.getPlayerName(networkplayerinfo1);
-
-          if (flag) {
-            mc.getTextureManager().bindTexture(networkplayerinfo1.getLocationSkin());
-            Gui.drawScaledCustomSizeModalRect(k2, l2, 8.0F, 8.0F, 8, 8, 8, 8, 64.0F, 64.0F);
-            EntityPlayer entityplayer = mc.theWorld.getPlayerEntityByUUID(networkplayerinfo1.getGameProfile().getId());
-
-            if (entityplayer != null && entityplayer.isWearing(EnumPlayerModelParts.HAT)) {
-              Gui.drawScaledCustomSizeModalRect(k2, l2, 40.0F, 8.0F, 8, 8, 8, 8, 64.0F, 64.0F);
-            }
-
-            k2 += 9;
-          }
-
-          if (networkplayerinfo1.getGameType() == WorldSettings.GameType.SPECTATOR) {
-            s1 = EnumChatFormatting.ITALIC + s1;
-            mc.fontRendererObj.drawStringWithShadow(s1, (float) k2, (float) l2, -1862270977);
-          } else {
-            mc.fontRendererObj.drawStringWithShadow(s1, (float) k2, (float) l2, -1);
-          }
-
-          mc.getTextureManager().bindTexture(indicators);
-          int indicatorRecIndex = RecMod.instance.isPlayerRecording(networkplayerinfo1.getGameProfile().getName()) ? IndexReference.ICON_RED_INDEX : IndexReference.ICON_GRAY_INDEX;
-          int indicatorStrIndex = RecMod.instance.isPlayerStreaming(networkplayerinfo1.getGameProfile().getName()) ? IndexReference.ICON_GREEN_INDEX : IndexReference.ICON_GRAY_INDEX;
-          drawTexturedModalRect(width / 2 + i2 / 2 + 1 - infooffset - 16, l2, indicatorRecIndex * 8, (int) Math.floor(indicatorRecIndex / 32) * 8 + IndexReference.RES_SSD, 8, 8);
-          drawTexturedModalRect(width / 2 + i2 / 2 + 1 - infooffset - 8, l2, indicatorStrIndex * 8, (int) Math.floor(indicatorStrIndex / 32) * 8 + IndexReference.RES_SSD, 8, 8);
-
-          if (scoreobjective != null && networkplayerinfo1.getGameType() != WorldSettings.GameType.SPECTATOR) {
-            int j4 = k2 + j + 1;
-            int i3 = j4 + i1;
-
-            if (i3 - j4 > 5) {
-              // Draw Scoreboard Values
-              to.drawScoreboardValues(scoreobjective, l2, networkplayerinfo1.getGameProfile().getName(), j4 - 24, i3 - 24, networkplayerinfo1);
-            }
-          }
-
-          // Draw Player Ping
-          to.drawPing(j1, k2 - (flag ? 9 : 0) - 18, l2, networkplayerinfo1);
-        }
-      }
-
-      if (list2 != null) {
-        l1 += k3 * 9 + 1;
-        drawRect(width / 2 - i2 / 2 - 1, l1 - 1, width / 2 + i2 / 2 + 1, l1 + list2.size() * mc.fontRendererObj.FONT_HEIGHT, Integer.MIN_VALUE);
-        drawRect(width / 2 - i2 / 2 - 1, l1 - 1, width / 2 + i2 / 2 + 1, l1 + list2.size() * mc.fontRendererObj.FONT_HEIGHT, Integer.MIN_VALUE);
-
-        for (iterator1 = list2.iterator(); iterator1.hasNext(); l1 += mc.fontRendererObj.FONT_HEIGHT) {
-          s = (String) iterator1.next();
-          j2 = mc.fontRendererObj.getStringWidth(s);
-          mc.fontRendererObj.drawStringWithShadow(s, (float) (width / 2 - j2 / 2), (float) l1, -1);
-        }
-      }
-    } else if (event.type == ElementType.ALL && RecMod.instance.showSelf && mc.currentScreen == null) {
+    if (event.type == ElementType.ALL && RecMod.instance.showSelf && mc.currentScreen == null) {
       int x;
       int y;
       switch (RecMod.instance.posMode) {
